631efb5e61096531c886dd817b4ce500bfd21b9e
Ashwinspill <118038285+Ashwinspill@users.noreply.github.com>
2023-12-18 22:38:16 +0530

    knn iris confusion matrix new dataset

--- Programs/exp4.py
+++ Programs/exp4.py
@@ -1,22 +1,34 @@
-from sklearn.datasets import load_iris
-from sklearn.metrics import accuracy_score,classification_report
-from sklearn.model_selection import train_test_split
+import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score,classification_report
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sea
 
-iris = load_iris()
+iris=load_iris()
+x=iris.data
+y=iris.target
 
-x = iris.data
-y = iris.target
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
+knn=KNeighborsClassifier(n_neighbors=7)
+knn.fit(x_train,y_train)
+v=knn.predict(x_test)
+accuracy_score=accuracy_score(y_test,v)
+print(accuracy_score)
+classification_report=classification_report(y_test,v)
+print(classification_report)
+confusion_matrix=confusion_matrix(y_test,v)
+print(confusion_matrix)
 
-x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=42)
-kn = KNeighborsClassifier(n_neighbors=7)
-kn.fit(x_train,y_train)
+plt.figure(figsize=(10,8))
+sea.heatmap(confusion_matrix,annot=True,cmap='Blues',fmt='g')
+plt.show()
 
-v = kn.predict(x_test)
-print(v)
 
-cl = classification_report(y_test,v)
-print(cl)
-
-ac = accuracy_score(y_test,v)
-print(ac)
+# new_data = np.array([5.1, 3.5, 1.4, 0.2]).reshape(1,-1)
+# predic=knn.predict(new_data)
+#
+# predicted=iris.target_names[predic[0]]
+# print(predicted)
