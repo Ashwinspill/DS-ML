ff6b7b031014952ff73d1a11d570d96218570674
Ashwinspill <118038285+Ashwinspill@users.noreply.github.com>
2023-12-18 13:29:03 +0530

    exp3

---
+++ Programs/exp3.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+catego = ["a","b","c","d"]
+values = [1,2,3,4]
+
+plt.bar(catego,values,color="skyblue")
+plt.xlabel("catego")
+plt.ylabel("values")
+plt.title("dafaff")
+plt.show()
